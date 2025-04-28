1e7728397447451c46c49557011878c3652d8ff5
Thiago Prates <thiago.prates@sptech.school>
2025-04-28 02:31:19 -0300

    Subindo a página inicial estilizada

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// efeitos da navbar 
+
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.menu');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elementos = document.querySelectorAll('.fade-in-scroll');
+
+    const observer = new IntersectionObserver((entradas, observer) => {
+        entradas.forEach(entrada => {
+            if (entrada.isIntersecting) {
+                entrada.target.classList.add('visible');
+                observer.unobserve(entrada.target);
+            }
+        });
+    }, {
+        rootMargin: '0px 0px -50px 0px', // Ajuste conforme necessário
+    });
+
+    elementos.forEach(el => observer.observe(el));
+});
+
+
+// funções do cadastro
+
+
+
+
+
+
+
+
