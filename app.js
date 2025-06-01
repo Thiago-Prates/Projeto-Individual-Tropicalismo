ed3f993d633390cba76bd1c406c6ae88597373db
Thiago Prates <thiago.prates@sptech.school>
2025-06-01 17:30:26 -0300

    Subindo ajustes

--- app.js
+++ app.js
@@ -43,10 +43,89 @@ app.listen(PORTA_APP, function () {
     #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
     ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
     ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
+    \n\n\n
+     e
+    \n\n\n                
+        ######                ###    #    
+        #     #  ####  #####   #    # #   
+        #     # #    # #    #  #   #   #  
+        ######  #    # #####   #  #     # 
+        #     # #    # #    #  #  ####### 
+        #     # #    # #    #  #  #     # 
+        ######   ####  #####  ### #     # 
+    \n\n\n                                                                                    
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
+
+
+// IMPLEMENTANDO BOBIA
+
+// importando os bibliotecas necessárias
+const { GoogleGenAI } = require("@google/genai");
+// const express = require("express");
+// const path = require("path");
+
+// carregando as variáveis de ambiente do projeto do arquivo .env
+require("dotenv").config();
+
+// configurando o servidor express
+// const app = express();
+const PORTA_SERVIDOR = process.env.PORTA;
+
+// configurando o gemini (IA)
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
+// configurando o servidor para receber requisições JSON
+app.use(express.json());
+
+// configurando o servidor para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "publicIA")));
+
+// configurando CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
+
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const modeloIA = chatIA.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `Você agora é um professor de música para inciantes, ensine me a tocar a música: ${mensagem}, apenas me ensine se for MPB `
+
+        });
+        const resposta = (await modeloIA).text;
+        const tokens = (await modeloIA).usageMetadata;
+
+        console.log(resposta);
+        console.log("Uso de Tokens:", tokens);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}                                                                           
+   
